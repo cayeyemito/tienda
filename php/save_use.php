52e17b088c7ffee99fd634446361a4b0de271257
vicksyy <victoriamarting7@gmail.com>
2025-03-18 12:52:06 +0100

    php cambios

--- php/save_use.php
+++ php/save_use.php
@@ -5,13 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"]
     $correo = $_POST["correo"]
 
-    $stmt = $conn-›prepare("INSERT INTO usuarios (nombre, correo) VALUES (P, ?)"):
+    $stmt = $conn-›prepare("INSERT INTO usuarios (nombre, correo) VALUES (?, ?)");
     $stmt-›bind_param("ss", $nombre, $correo);
 
     if ($stmt-›execute()) {
         echo "Usuario registrado correctamente.";
     } else {
-        echo "Error al registrar: " . $conn-›error;  
+        echo "Error al registrar: " . $conn-›error;
     }
 
     $stmt->close():

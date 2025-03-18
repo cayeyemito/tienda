4b39bf63dc61d2a5217898e8d8c907694689528b
vicksyy <victoriamarting7@gmail.com>
2025-03-18 12:53:10 +0100

    cambios flechitas php

--- php/save_use.php
+++ php/save_use.php
@@ -5,15 +5,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"]
     $correo = $_POST["correo"]
 
-    $stmt = $conn-›prepare("INSERT INTO usuarios (nombre, correo) VALUES (?, ?)");
-    $stmt-›bind_param("ss", $nombre, $correo);
+    $stmt = $conn->prepare("INSERT INTO usuarios (nombre, correo) VALUES (?, ?)");
+    $stmt->bind_param("ss", $nombre, $correo);
 
-    if ($stmt-›execute()) {
+    if ($stmt->execute()) {
         echo "Usuario registrado correctamente.";
     } else {
-        echo "Error al registrar: " . $conn-›error;
+        echo "Error al registrar: " . $conn->error;
     }
 
     $stmt->close():
-    $conn-›close():
+    $conn->close():
 }

8cf97869cffdec6d77554f736f201be0fd0cee40
= <=>
2025-03-18 12:57:47 +0100

    Added js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.getElementById("userForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("correo").value;
+    
+    fetch("./php/save_user.php", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `nombre=${nombre}&correo=${correo}`
+    })
+    .then(response => response.text())
+    .then(data => {
+        alert(data);
+        document.getElementById("userForm").reset();
+        loadUsers();
+    });
+});
+
+function loadUsers() {
+    fetch("./php/get_users.php")
+    .then(response => response.json())
+    .then(users => {
+        let table = document.getElementById("userTable");
+        table.innerHTML = "";
+        users.forEach(user => {
+            let row = `<tr><td>${user.id}</td><td>${user.nombre}</td><td>${user.correo}</td></tr>`;
+            table.innerHTML += row;
+        });
+    });
+}
+
+window.onload = loadUsers;

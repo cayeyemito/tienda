a074eab3d3a3220a6be536aebe6ad79fe9ff01fd
= <=>
2025-03-18 13:11:36 +0100

    asas

--- php/get_users.php
+++ php/get_users.php
@@ -4,6 +4,10 @@ include 'db.php';
 $sql = "SELECT * FROM usuarios";
 $result = $conn->query($sql);
 
+if (!$result) {
+    die('Query failed: ' . $conn->error);
+}
+
 $users = [];
 while ($row = $result->fetch_assoc()) {
     $users[] = $row;

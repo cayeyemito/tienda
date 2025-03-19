fb2a2060ccad3b93e2cc4a4021e93c0b1d5c1233
vicksyy <victoriamarting7@gmail.com>
2025-03-19 09:30:55 +0100

    png edit

--- php/delete_user.php
+++ php/delete_user.php
@@ -1,11 +1,11 @@
 <?php
 include 'db.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id"])) {
     $userID = $_POST["id"];
 
     $stmt = $conn->prepare("DELETE FROM Usuarios WHERE id = ?");
-    $stmt->bind_param("int", $userID);
+    $stmt->bind_param("i", $userID);
 
     if ($stmt->execute()) {
         echo "Usuario eliminado correctamente.";
